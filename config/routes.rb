862bb66e56ad1e7e8b68fdcc0596de66ea6274df
Hoang Nguyen <nguyenhua.hcm@gmail.com>
2025-08-25 22:34:11 +0700

    Add dashboards controller and configure signup routes instead of devise default

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,17 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  devise_for :users, skip: [:sessions]
+  authenticated :user do
+    root to: 'dashboards#show', as: :authenticated_root
+  end
+
+  devise_for :users, skip: [:sessions, :registrations]
   as :user do
     get 'login', to: 'devise/sessions#new', as: :new_user_session
     post 'login', to: 'devise/sessions#create', as: :user_session
     delete 'logout', to: 'devise/sessions#destroy', as: :destroy_user_session
+    get 'signup', to: 'devise/registrations#new', as: :new_user_registration
+    post 'signup', to: 'devise/registrations#create', as: :user_registration
   end
+  root to: redirect('/login')
 end
